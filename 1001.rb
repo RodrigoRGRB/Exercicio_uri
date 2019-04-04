b6db665455eaba9539fe6590e0a0c74b58cbe201
Rodrigo Gabriel <rodrigorgrb@gmail.com>
2019-04-04 17:47:44 +0000

    1001 versão melhorada

--- 1001.rb
+++ 1001.rb
@@ -1,8 +1,16 @@
-x=gets.chomp
-A=x.to_i
-x=gets.chomp
-B=x.to_i
-X=A+B
-print "X = "
-print X
-print "\n"
+a = gets.to_i
+b = gets.to_i
+
+x = a + b
+
+puts "X = #{x}"
+
+
+# x=gets.chomp
+# A=x.to_i
+# x=gets.chomp
+# B=x.to_i
+# X=A+B
+# print "X = "
+# print X
+# print "\n"
